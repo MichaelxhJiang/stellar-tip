8eec0a7ae96539a2c1bac90fe8b5db7ee4bd601f
Mufeez Amjad <mufeez.amjad@outlook.com>
2020-03-08 00:35:05 -0500

    memoID generation

--- server/routes/api/transactions.js
+++ server/routes/api/transactions.js
@@ -1,20 +1,34 @@
 const express = require('express')
 const router = express.Router()
-const { getTransactionHistory } = require('../../controllers/stellar')
+const { getTransactionHistory, createTransactionUri } = require('../../controllers/stellar')
 
 const Transaction = require('../../models/Transaction');
 
 router.post('/', async (req, res) => {
     const data = req.body;
+    const { sender, receiver, asset, amount, url } = data || {}
+    var firstPart = (Math.random() * 46656) | 0;
+    var secondPart = (Math.random() * 46656) | 0;
+    firstPart = ("000" + firstPart.toString(36)).slice(-3);
+    secondPart = ("000" + secondPart.toString(36)).slice(-3);
+    const id = firstPart + secondPart;
+    const memoID = id + sender.substring(sender.length - 8) + receiver.substring(receiver.length - 8);
     try {
-        await Transaction.create(data)
+        await Transaction.create({
+            sender, 
+            receiver,
+            memoID,
+            url
+        })
+        const uri = createTransactionUri(sender, receiver, asset, amount, memoId)
+        res.json(uri);
         return res.sendStatus(200);
     } catch (err) {
         return res.status(500).json(err);
     }
 });
 
-router.get('/', async (req, res) => {
+router.get('/history', async (req, res) => {
     const data = req.body
     const { payerPublicKey } = data
     try {

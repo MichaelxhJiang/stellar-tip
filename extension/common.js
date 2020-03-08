e2d846d0e81a18ab5b3103684f5abaa027dd79ae
Mufeez Amjad <mufeez.amjad@outlook.com>
2020-03-08 06:13:27 -0400

    updated form design

--- extension/common.js
+++ extension/common.js
@@ -1,46 +1,55 @@
 const dialogHTML = `
 <div class="stellar-tip-dialog" style="display:none;">
-  <div class=stellar-tip-dialog-inner>
-    <form>
-    <table>
-      <tr>
-      <td>
-        <label for="display_name">Display name&nbsp;</label>
-      </td>
-      <td>
-        <input type="text" name="display_name" style="width: 12em;" />
-      </td>
-      </tr>
-      <tr>
-      <td>
-        <label for="message">Message&nbsp;</label>
-      </td>
-      <td>
-        <input type="text" name="message" placeholder="(optional)" style="width: 12em;" />
-      </td>
-      </tr>
-      <tr>
-      <td>
-        <label for="amount">Amount&nbsp;</label>
-      </td>
-      <td>
-        <input type="text" name="amount" style="width:7.75em;" />
-        <select name="asset" style="width:4em;">
-        <option value="XLM">XLM</option>
-        <option value="USD">USD</option>
-        <option value="USD">CAD</option>
-        <option value="USD">EUR</option>
-        </select>
-      </td>
-      </tr>
-      <tr>
-      <td colspan="2">
-        <button>Send Tip</button>
-      </td>
-      </tr>
-    </table>
-    </form>
-  </div>
+<div class='stellar-tip-dialog-inner'>
+<form class='modal'>
+    <header class='header'>
+        <div class='card-type'>
+            <a class='card'>
+                <b>XLM</b>
+            </a>
+            <a class='card active'>
+              <b>USD</b>
+            </a>
+            <a class='card'>
+              <b>EUR</b>
+            </a>
+            <a class='card'>
+              <b>CAD</b>
+            </a>
+        </div>
+    </header>
+    <div class='content'>
+        <div class='form'>
+            <div class='form-row'>
+                <div class='input-group'>
+                    <label for=''>Alias</label>
+                    <input placeholder='' type='text'>
+                </div>
+            </div>
+            <div class='form-row'>
+                <div class='input-group'>
+                    <label for=''>Public Address</label>
+                    <input maxlength='56' placeholder='XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX' type='number'>
+                </div>
+            </div>
+            <div class='form-row'>
+                <div class='input-group'>
+                    <label for=''>Message</label>
+                    <input placeholder='' type='text'>
+                </div>
+                <div class='input-group'>
+                    <label for=''>Amount</label>
+                    <input placeholder='' type='number'>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+    <div class="footer">
+      <a class='button'>Send Tip</a>
+    </div>
+</form>
+</div>
 </div>
 `
 

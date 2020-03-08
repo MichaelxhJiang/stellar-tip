36777aa54513469f12727041d9a7bc4f819d13d9
Ethan Guo <ethguo@gmail.com>
2020-03-08 00:04:30 -0500

    refactor and fix things

--- extension/youtube.js
+++ extension/youtube.js
@@ -4,63 +4,24 @@ const buttonHTML = `
 <button class="stellar-tip-button" onclick="">
   <img src="https://stealthex.io/db-assets/coins/icons/color/8laQ7QbGctJIdKLm.svg">
   <span>Stellar Tip</span>
-  <div class="stellar-tip-dialog" style="display:none;">
-    <div class=stellar-tip-dialog-inner>
-      <form>
-        <table>
-          <tr>
-            <td>
-              <label for="display_name">Display name&nbsp;</label>
-            </td>
-            <td>
-              <input type="text" name="display_name" style="width: 12em;" />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <label for="message">Message&nbsp;</label>
-            </td>
-            <td>
-              <input type="text" name="message" placeholder="(optional)" style="width: 12em;" />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <label for="amount">Amount&nbsp;</label>
-            </td>
-            <td>
-              <input type="text" name="amount" style="width:7.75em;" />
-              <select name="asset" style="width:4em;">
-                <option value="XLM">XLM</option>
-                <option value="USD">USD</option>
-                <option value="USD">CAD</option>
-                <option value="USD">EUR</option>
-              </select>
-            </td>
-          </tr>
-          <tr>
-            <td colspan="2">
-              <button>Send Tip</button>
-            </td>
-          </tr>
-        </table>
-      </form>
-    </div>
-  </div>
+  ${dialogHTML}
 </button>
 `
 
 function addTipButton() {
-  if ($('.stellar-tip-button').length) {
+  var $tipButton = $('.stellar-tip-button');
+
+  if ($tipButton.length) {
     console.log("found existing tip button, will reassign receiver");
-    // will have to reassign receiver stellar address in dialog
+    $tipButton.find(".stellar-tip-dialog").hide();
+    //todo: have to reassign receiver stellar address in dialog
     return;
   }
   console.log("trying to addTipButton");
 
   $('#meta-contents #subscribe-button').before(buttonHTML);
 
-  var $tipButton = $('.stellar-tip-button');
+  $tipButton = $('.stellar-tip-button'); // This line is necessary dont delete it
 
   if ($tipButton.length) {
     console.log("addTipButton succeeded");

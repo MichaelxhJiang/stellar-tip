eb066b8c3c0fdb023813a678a17416b1dc06b10b
Marcel O'Neil <marcel@marceloneil.com>
2020-03-08 08:43:23 -0400

    use uri signing key

--- server/routes/api/well-known.js
+++ server/routes/api/well-known.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 router.get('/stellar.toml', async(req, res) => {
   res.set('Content-Type', 'text/plain')
-  res.send(`SIGNING_KEY="${process.env.STELLAR_PUBLIC_KEY}"`)
+  res.send(`URI_REQUEST_SIGNING_KEY="${process.env.STELLAR_PUBLIC_KEY}"`)
 });
 
 module.exports = router;
